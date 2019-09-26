2eb1a27198cb8ce4d88dc033e3b8aaca95ae7162
john lam <johnlam90@gmail.com>
2019-09-26 15:00:23 -0500

    Update static-routing.sh

--- static-routing.sh
+++ static-routing.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 
-#Custom static routes to for bidirectional communications while specifying interfaces 
+#Custom static routes to for bidirectional communications while specifying interfaces. A config Map must be created and mounted using an init container 
+
 
 #Example 
 
 #ip route add 10.46.87.6/32 via 10.46.90.225 dev net1
+#ip route add 10.46.87.5/32 via 10.46.88.141 dev net2 
